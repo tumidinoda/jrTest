5ca5938bd6f930ac5321839fe9b07aab2a38d94a
robert@Mac <robert.jonas@gmx.at>
2017-01-18 12:36:33 +0100

    Initial Commit

---
+++ jrTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jrTest
+//
+//  Created by Robert Jonas on 18.01.17.
+//  Copyright © 2017 jr-soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
